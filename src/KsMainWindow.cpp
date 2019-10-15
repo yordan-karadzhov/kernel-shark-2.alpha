716068c787f3749d4f83ce078f45f6cd934bfd41
Yordan Karadzhov (VMware) <y.karadz@gmail.com>
2019-10-15 22:59:33 +0300

    GUI: Fix simple typo in the "File" menu.
    Signed-off-by: Yordan Karadzhov (VMware) <y.karadz@gmail.com>

--- src/KsMainWindow.cpp
+++ src/KsMainWindow.cpp
@@ -49,7 +49,7 @@ KsMainWindow::KsMainWindow(QWidget *parent)
   _appendAction("Append Trace File", this),
   _restoreSessionAction("Restore Last Session", this),
   _importSessionAction("Import Session", this),
-  _exportSessionAction("Export Sassion", this),
+  _exportSessionAction("Export Session", this),
   _quitAction("Quit", this),
   _importFilterAction("Import Filter", this),
   _exportFilterAction("Export Filter", this),
